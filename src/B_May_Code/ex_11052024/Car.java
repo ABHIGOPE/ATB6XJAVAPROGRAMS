4320eab42af168d39226df77757efd5a41800715
ABHIGOPE <gopeabhishek165@gmail.com>
2024-07-14 11:24:35 +0530

    Today exercise

---
+++ src/B_May_Code/ex_11052024/Car.java
@@ -0,0 +1,17 @@
+package B_May_Code.ex_11052024;
+
+public class Car {
+    String name;
+    String size;
+    String tyres;
+    String model;
+    String color;
+    String transmissoinType;
+
+
+
+    // behav / methods/  functionaities
+    void reverse(){}
+    void speedTop(){}
+    void musicSystem(){
+}}
